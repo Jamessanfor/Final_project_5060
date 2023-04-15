16f9916f479a2c3080ceee4725da5176de89bda7
Jamessanfor <78274711+Jamessanfor@users.noreply.github.com>
2023-04-15 00:06:35 -0600

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -10,20 +10,20 @@ int main() {
     reverse(Possible_hand.begin(), Possible_hand.end());
     vector<player> players;
     vector<card> c;
-    int head = 2;//number of players
+    int head = 3;//number of players
 
     deck D = deck();
-
     D.shuffle();
     D.shuffle();
     D.makegame(players,head );
     D.flop(players);
     D.turn(players);
     D.river(players);
-    int sim = 1000;
+    int sim = 100000;
     int flag = 0;
     optimal_stopper op_stop = optimal_stopper(D, players, 0);
     genaric_player gen_player = genaric_player(1);
+    Explore_Exploit_player exp_exp = Explore_Exploit_player(2);
     while (sim) {
         //game
         D.shuffle();
@@ -31,12 +31,16 @@ int main() {
         D.deal(players);
         op_stop.raise_meet_fold(players);
         gen_player.raise_meet_fold(players);
+        
+        exp_exp.raise_meet_fold(players);
+
         flag = D.betting_stage(players);
         if (!flag) {
             D.flop(players);
             op_stop.raise_meet_fold(players);
             gen_player.raise_meet_fold(players);
-
+            exp_exp.raise_meet_fold(players);
+        
             flag = D.betting_stage(players);
         }
         
@@ -44,7 +48,7 @@ int main() {
             D.turn(players);
             op_stop.raise_meet_fold(players);
             gen_player.raise_meet_fold(players);
-
+            exp_exp.raise_meet_fold(players);
         
             flag = D.betting_stage(players);
         }
@@ -54,19 +58,25 @@ int main() {
 
             op_stop.raise_meet_fold(players);
             gen_player.raise_meet_fold(players);
+            exp_exp.raise_meet_fold(players);
             D.divy_up_winnings(players);
+        
+            exp_exp.check_set(players);
         }
+
         flag = 0;
         //D.print_playerhands(players);
        // int enter;
         sim--;
         
-       //cout<<players[0].money<<endl;
+       // cout<<players[0].money<<endl;
        // cout << players[1].money << endl;
+       // cout << players[2].money << endl;
     }
     cout << D.pot << endl;
     cout << players[0].money << endl;
     cout << players[1].money << endl;
+    cout << players[2].money << endl;
 
         return 0;
 }
--- player_type.h
+++ player_type.h
@@ -142,28 +142,89 @@ public:
 
 
 
-class Explore_Exploit {
+class Explore_Exploit_player {
 
 public:
-    int stopping_card = 0;
-    int stopping_hand = 0;
+
     int player_index = 0;
+    int money = 0;
+    int rank_player= 0;
+    int hand_player = 0;
 
+    vector<int> hand_map;
+    vector<int> rank_map;
+    void set_prob_hand(vector<int> &map) {
+    
+        for (int i = 0; i < map.size(); i++) {
+            if (i < ((map.size()) / 2))
+                map[i] = 0.5;
+            else map[i] = i / map.size();
+        }
 
-    Explore_Exploit(int id) {
+    }
+    void set_prob(vector<int>& map) {
+
+        for (int i = 0; i < map.size(); i++) {
+                map[i] = i / map.size();
+        }
+
+    }
+    Explore_Exploit_player(int id) {
         player_index = id;
+        hand_map.resize(Possible_hand.size());
+        rank_map.resize(ranks.size());
+        //set inital prob
+        set_prob_hand(hand_map);
+        set_prob(rank_map);
+    }
+    void set_money(vector<player>& players) {
+        money = players[player_index].money;
+
+    }
+    void check_money(vector<player>& players) {
+        
+       if( money > players[player_index].money){
+           
+           rank_map[players[player_index].high] = (rank_map[players[player_index].high] >0) ? (rank_map[players[player_index].high]-.01) : 0;
+           hand_map[players[player_index].curr_hand] = (hand_map[players[player_index].curr_hand] > 0) ? (hand_map[players[player_index].curr_hand] - .01) : 0;
+
+       }
+       else {
+           
+           rank_map[players[player_index].high] = (rank_map[players[player_index].high] < 1) ? ((rank_map[players[player_index].high] + .01)) : 1;
+           hand_map[players[player_index].curr_hand] = (hand_map[players[player_index].curr_hand] < 1) ? ((hand_map[players[player_index].curr_hand] + .01)) : 1;
+
+       }
+
+    }
+    void check_set(vector<player>& players){
+        
+        check_money(players);
+        
+        set_money(players);
+    }
+
+    void set_hand(vector<player>& players) {
+        rank_player = players[player_index].high;
+        hand_player = players[player_index].curr_hand;
         
     }
 
 
-
     void raise_meet_fold(vector<player>& players) {
-        //check current hand
-        if (players[player_index].curr_hand >= (stopping_hand - 2) || players[player_index].high >= (stopping_card - 2) || dist(gen) > 6000) {
+
+
+        if (players[player_index].curr_hand != hand_player || players[player_index].high != rank_player) {
+            set_hand(players);
+        }
+
+
+        
+        if ( dist(gen)  <= (hand_map[players[player_index].curr_hand] * 10000) || dist(gen) <= (rank_map[players[player_index].curr_hand] * 10000)) {
             players[player_index].action = raise;
 
         }
-        else if (players[player_index].curr_hand >= (1) || players[player_index].high >= (stopping_card - 4) || dist(gen) > 9000) {
+        else if (dist(gen) <= (hand_map[players[player_index].curr_hand] * 15000) || dist(gen) <= (rank_map[players[player_index].curr_hand] * 15000)) {
             players[player_index].action = meet;
 
         }
